5fcf0d798085e0be584ce371fa04595f52bf7ea6
Senti0027 <5B0G0027@stust.edu.tw>
2024-03-08 10:47:32 +0800

    main03

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_03 = New System.Windows.Forms.Button()
+        Me.txt_03 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn_03
+        '
+        Me.btn_03.Location = New System.Drawing.Point(247, 36)
+        Me.btn_03.Name = "btn_03"
+        Me.btn_03.Size = New System.Drawing.Size(200, 37)
+        Me.btn_03.TabIndex = 0
+        Me.btn_03.Text = "第三題"
+        Me.btn_03.UseVisualStyleBackColor = True
+        '
+        'txt_03
+        '
+        Me.txt_03.Location = New System.Drawing.Point(252, 176)
+        Me.txt_03.Multiline = True
+        Me.txt_03.Name = "txt_03"
+        Me.txt_03.Size = New System.Drawing.Size(176, 109)
+        Me.txt_03.TabIndex = 1
+        Me.txt_03.Text = "Move your body let it dance again"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txt_03)
+        Me.Controls.Add(Me.btn_03)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_03 As Button
+    Friend WithEvents txt_03 As TextBox
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btn_03_Click(sender As Object, e As EventArgs) Handles btn_03.Click
+        Dim m0, m1, m2, m3, m4
+        FileOpen(1, "1060303.SM", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+        For i = 1 To m0
+            If m0 Mod i = 0 Then m1 += 1
+        Next
+        If m1 = 2 Then
+            txt_03.Text = "第三題結果:" & m0 & "is a prime number."
+        Else
+            txt_03.Text = "第三題結果:" & m0 & "is not a prime number."
+
+        End If
+    End Sub
+End Class
